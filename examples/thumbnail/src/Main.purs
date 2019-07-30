c7a9879945696a084e0baabfa81bf034de6ba903
John Watson <john.watson@gmx.co.uk>
2019-07-30 12:13:07 +0100

    Add thumbnail example

---
+++ examples/thumbnail/src/Main.purs
@@ -0,0 +1,57 @@
+module Examples.Thumbnail.Main where
+
+import Prelude (bind, pure)
+import Effect (Effect)
+import Data.Either (Either(..))
+import VexFlow.Score (createScore, renderScore, initialiseCanvas)
+import VexFlow.Types (Config)
+import VexFlow.Abc.Alignment (justifiedScoreConfig, rightJustify)
+import VexFlow.Abc.Utils (canvasHeight)
+import Data.Abc.Parser (parse)
+import Data.Abc.Metadata (thumbnail)
+import Data.Abc (AbcTune)
+import Examples.Thumbnail.Texts (augustsson, cig, ewa, fastan, smalandPolska,
+                                gustavPersson, voltaContinuationSample,
+                                keyChangeSample, meterChangeSample,
+                                continuationSample, emptyBarSample)
+
+canvasWidth :: Int
+canvasWidth = 1000
+
+canvasDepth :: Int
+canvasDepth = 200
+
+scale :: Number
+scale = 0.8
+
+configure :: AbcTune -> Config
+configure tune =
+  { canvasDivId : "canvas"
+  , canvasWidth : canvasWidth
+  , canvasHeight : canvasHeight tune
+  , scale : 0.8
+  }
+
+defaultConfig :: Config
+defaultConfig =
+  { canvasDivId : "canvas"
+  , canvasWidth : canvasWidth
+  , canvasHeight : canvasDepth
+  , scale : scale
+  }
+
+main :: Effect Boolean
+main =
+  let
+    eAbcTune = parse ewa
+  in
+    case eAbcTune of
+      Right abcTune -> do
+        let
+          unjustifiedScore = createScore defaultConfig (thumbnail abcTune)
+          score = rightJustify canvasWidth scale unjustifiedScore
+          config = justifiedScoreConfig score defaultConfig
+        _ <- initialiseCanvas config
+        renderScore config score
+      _ ->
+        pure false
---
+++ examples/thumbnail/src/Texts.purs
@@ -0,0 +1,125 @@
+module Examples.Thumbnail.Texts where
+
+import Prelude ((<>))
+
+augustsson :: String
+augustsson =
+  "X:1\r\n"
+  <> "T:Engelska efter Albert Augustsson\r\n"
+  <> "N:From the playing of Albert Augustsson, Bohuslän county.\r\n"
+  <> "M:4/4\r\n"
+  <> "R:Engelska\r\n"
+  <> "S:Orust\r\n"
+  <> "Z:John Watson 24/01/2015\r\n"
+  <> "L:1/8\r\n"
+  <> "K:A\r\n"
+  <> "A>c|: e2f2 efed | c2a2 e3d | cedc BdcB | Aced cBAc |\r\n"
+  <> "e2f2 efed | c2a2 e3d | cedc BdcB | A4 A>AA>B :|\r\n"
+  <> "|: e2e2 e2de | f2ed B3c | d3c d2cd | e3d cdBc |\r\n"
+  <> "A2a2 a2gf | e2f2 e3d | cedc BdcB |1 A4 A>AA>B :|2 [A4E4] [A4E4] |\r\n"
+
+fastan :: String
+fastan =
+  "T: Fastan\r\n"
+  <> "R: Polska\r\n"
+  <> "M: 3/4\r\n"
+  <> "K: F\r\n"
+  <> "L: 1/16\r\n"
+  <> "| (3A4F4G4 A2B2 | (3:4:3c2d2B4c4 A2F2 | (3F4E4D4 B,2D2 | EA3 A8- |\r\n"
+  <> "| (3A4F4G4 A2B2 | (3:4:3c2d2B4c4 A2F2 | (3F4E4D4 G2A2 | AF3 F8- |\r\n"
+  <> "| (3:5:3F4B4cBA2 B2d2 | ge3 c4 A4- | (3:5:3A4B4cBA2 B2d2 | de3 c8- |\r\n"
+  <> "| (3:5:3F4B4cBA2 B2d2 | (3:4:3g2a2f4g4 e4- | (3:c4B4A4 F2G2 | ef3 F8 |\r\n"
+
+ewa :: String
+ewa =
+  "T: Ewa\r\n"
+  <> "R: polska\r\n"
+  <> "C: Josefina Paulson\r\n"
+  <> "M: 9/8\r\n"
+  <> "L: 1/8\r\n"
+  <> "Q: 3/8=120\r\n"
+  <> "K: G\r\n"
+  <> "|: D2E D3 B3- | B2A B2c BAB | cB2 (2:3AG FEF | GD2 C3 B,3 | \r\n"
+  <> "| D2E D3 B3- | B2A B2c BAB | cB2 (2:3AG FEF | G3 G6 :| \r\n"
+  <> "|: dBd g2f (2:3ed | cB2 (2:3AG F2G | (2:3AB cAc BGB | AF2  D6 |\r\n"
+  <> "| dBd g2f (2:3ed |  cB2 (2:3AG F2G | (2:3AB cAc BGB | AF2  G6 :|\r\n"
+
+cig :: String
+cig =
+  "X: 1\r\n"
+  <> "T: C i G, Grind Hans Jässpôdspolska\r\n"
+  <> "Z: Christine Dyer\r\n"
+  <> "R: Polska\r\n"
+  <> "O: Rättvik, Dalarna\r\n"
+  <> "M: 3/4\r\n"
+  <> "L: 1/8\r\n"
+  <> "K:Gmaj\r\n"
+  <> "|: D2 G2 A>B | (3c2B2G2 E2- | E2 c>B A>G | F/2G/2A- AF D2- |\r\n"
+  <> "D2 G2 A>B | (3c2B2G2 E2- | E2 c>B AG |1 (3FDF G4 :|2  (3FDF G3 D |\r\n"
+  <> "|: G>B dc B>A | G<c ed c>B | A>B c/2d/2c/2A/2 B>G |F/2G/2A- AF D>D |\r\n"
+  <> "G>B dc B>A | G<c ed c>B | A>B c/2d/2c/2A/2 B>G |1 FD/2F/2 G3 D :|2 FD/2F/2 G4 |\r\n"
+  <> "|  |\r\n"
+
+smalandPolska :: String
+smalandPolska =
+  "X:1\r\n"
+  <> "T:Polska från Småland\r\n"
+  <> "M:3/4\r\n"
+  <> "L:1/16\r\n"
+  <> "K:Bmin\r\n"
+  <> "R:polska\r\n"
+  <> "|: B4 A4 (B4 | d2)f2 e2dc c2(d2 |B2)B2 A2A2 B2B2 |d2f2 e2dc d4 |\r\n"
+  <> "F2GA B2AB c2Bc |d2cd (ed)cB A2(F2 | F2)GA B2AB c2Bc |d2cd (ed)cB A2(F2 |\r\n"
+  <> "F2)GA (B2c2) d3B |(B2A2) B8 :|\r\n"
+  <> "K:Amaj\r\n"
+  <> "|: f4 e4 (f4 |g2)a2 b2ag g2(a2 |f2)f2 e2e2 f2f2 |g2a2 b2ag a4 |\r\n"
+  <> "c2de f2ef g2fg |a2ga (ba)gf e2c2 | c2de f2ef g2fg |a2ga (ba)gf e2c2 |\r\n"
+  <> "c2de f2g2 a3f |f2e2 f8 :|\r\n"
+
+gustavPersson :: String
+gustavPersson =
+  "X: 1\r\n"
+  <> "T: Polska efter Gustav Persson\r\n"
+  <> "R: polska\r\n"
+  <> "M: 9/8\r\n"
+  <> "L: 1/16\r\n"
+  <> "K:Dmin\r\n"
+  <> "|: F4A2 A4G2 E4F2 | D4F2 (E4FE) D4d2 | c4e2 e4d2 c4=B2 |1 A4c2 =B6 A4G2 :|\r\n"
+  <> "|2 A4c2 =B6 A6 || A2=B2^c2 d4e2 f4d2 | e2^f2^g2 a4^c2 a4^c2 | A4d2 d6-d4^c2 |\r\n"
+  <> "A3=B3 ^c3d3 e3f3 | f2e2f2 a6 f4a2 | a4g2 (e4fe) ^c4e2 | e4d2 (=B4cB) A6 |\r\n"
+  <> "A2=B2^c2 d4e2 f4d2 | e2^f2^g2 a4^c2 a4^c2 | A4d2 d12 |\r\n"
+  <> "|: (A4=B^c) d3c3 A6 | A3G3 E3F3 D6 | (4d2e2d2^c2 d3f3 e3d3 | c6 A12 |\r\n"
+  <> "| (A4=B^c) d3c3 A6 | A3G3 E3F3 D3B3 | A3G3 E3F3 D3F3 | (E4FE) D12 :|\r\n"
+
+keyChangeSample :: String
+keyChangeSample =
+  "K: D\r\n"
+  <> "M: 4/4\r\n"
+  <> "| A B c |[K: A] F |\r\n"
+  <> "| f g a |\r\n"
+
+meterChangeSample :: String
+meterChangeSample =
+  "K: D\r\n"
+  <> "M: 4/4\r\n"
+  <> "| A B c |\r\n"
+  <> "M: 3/4\r\n"
+  <> "| f g a |\r\n"
+
+continuationSample :: String
+continuationSample =
+  "| ABc |\\\r\n| def |\r\n"
+
+emptyBarSample :: String
+emptyBarSample =
+  "| ABc | | def |\r\n"
+
+voltaContinuationSample :: String
+voltaContinuationSample =
+    "X:1\r\n"
+    <> "T: volta continued across staves\r\n"
+    <> "M: 3/4\r\n"
+    <> "L: 1/8\r\n"
+    <> "K: D\r\n"
+    <> "| AB cd ef |1 bb cc dd :|2 dd cc bb | AA BB cc |\r\n"
+    <> "| dd ee ff || g6 |\r\n"
