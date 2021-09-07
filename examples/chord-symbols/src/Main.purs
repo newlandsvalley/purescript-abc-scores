61ec398b424aa0cf477e367b508cc355b78de5da
John Watson <john.watson@gmx.co.uk>
2021-09-07 12:26:34 +0100

    Add chord symbol examples
    most of which point out the inadequacy of the current implementation

---
+++ examples/chord-symbols/src/Main.purs
@@ -0,0 +1,67 @@
+module Examples.ChordSymbols.Main where
+
+import Examples.ChordSymbols.Texts
+
+import Data.Abc (KeySignature, MeterSignature)
+import Data.Abc.Parser (parse)
+import Data.Either (Either(..))
+import Data.Int (round, toNumber)
+import Data.Maybe (Maybe(..))
+import Data.Rational ((%))
+import Data.Tuple (Tuple(..))
+import Effect (Effect)
+import Prelude (Unit, bind, pure, unit, ($), (/))
+import VexFlow.Abc.Beat (beatDuration)
+import VexFlow.Score (Renderer, initialiseCanvas, renderTuneAtStave)
+import VexFlow.Types (Config, AbcContext, defaultConfig, staveIndentation)
+
+canvasWidth :: Int
+canvasWidth = 1200
+
+canvasHeight :: Int
+canvasHeight = 1600
+
+config :: Config
+config =
+  defaultConfig 
+    { width = canvasWidth
+    , height = canvasHeight
+    , titled = false 
+    , showChordSymbols = true
+    }
+
+abcContext :: MeterSignature -> KeySignature -> Int -> AbcContext
+abcContext (Tuple numerator denominator) keySignature staveNo =
+  { timeSignature : { numerator, denominator }
+  , keySignature : keySignature
+  , mTempo : Nothing
+  , unitNoteLength : ( 1 % 16)
+  , staveNo : Just staveNo
+  , accumulatedStaveWidth : staveIndentation
+  , isMidVolta : false
+  , isNewTimeSignature : false
+  , maxWidth : round $ (toNumber canvasWidth / config.scale)
+  , pendingRepeatBegin: false
+  , beatDuration: beatDuration { numerator, denominator }
+  , showChordSymbols: config.showChordSymbols
+  }
+
+displayAtStave :: Renderer -> String -> Int -> Effect Boolean
+displayAtStave renderer text staveNo =
+  let
+    eAbcTune = parse text
+  in
+    case eAbcTune of
+      Right abcTune -> do
+        renderTuneAtStave staveNo config renderer abcTune
+      _ ->
+        pure false
+
+main :: Effect Unit
+main = do
+  renderer <- initialiseCanvas config
+  _ <- displayAtStave renderer tempoMarking 0
+  _ <- displayAtStave renderer tuplet 1
+  _ <- displayAtStave renderer basic 2
+  _ <- displayAtStave renderer voltaBrackets 3
+  pure unit
---
+++ examples/chord-symbols/src/Texts.purs
@@ -0,0 +1,44 @@
+module Examples.ChordSymbols.Texts where
+
+import Prelude ((<>))
+
+-- The tempo marking must be against stave 0, bar o but for 
+-- some reason, it does not display here
+tempoMarking :: String
+tempoMarking =
+    "X: 1\r\n"
+    <> "T: chord symbols plus tempo marking\r\n"
+    <> "Q: 1/4=120\r\n"
+    <> "M: 3/4\r\n"
+    <> "L: 1/8\r\n"
+    <> "K: C\r\n"
+    <> "\"C\" c2 cd ef | gg/a/ ge ca | \"F\" a>g \"Em\" ge \"Am\" c2 |\r\n"      
+
+basic :: String
+basic =
+    "X:1\r\n"
+    <> "T: basic chord symbols\r\n"
+    <> "M: 3/4\r\n"
+    <> "L: 1/8\r\n"
+    <> "K: G\r\n"
+    <> "\"G\"G2G>B \"D7\"A>F | \"G\"G2B>dg2 |\r\n"
+
+tuplet :: String 
+tuplet =
+    "X:1\r\n"
+    <> "T:chord symbols plus tuplet\r\n"
+    <> "R:Polska\r\n"
+    <> "L:1/8\r\n"
+    <> "M:3/4\r\n"
+    <> "K:Bm\r\n"
+    <> "\"Bm\"(3FGFB2F>B | \"F#7\"^A>B c2 e>f | \"Bm\"(3ded (3cdc ^A>B | \"F#7\"c/d/c/^A/ B>A F2 |\r\n"
+
+
+voltaBrackets :: String
+voltaBrackets =
+    "X:1\r\n"
+    <> "T: chord symbols plus volta brackets\r\n"
+    <> "M: 2/4\r\n"
+    <> "L: 1/16\r\n"
+    <> "K: D\r\n"
+    <> " \"G\" B2BG BGBG |1 \"D\" B2A2 A4 :|2 \"D\" B2A2 A3c |\r\n"
