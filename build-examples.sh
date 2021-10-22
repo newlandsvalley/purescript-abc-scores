9dfcb9b4cce97a78d0f9030dc1075f36269a14e1
John Watson <john.watson@gmx.co.uk>
2021-10-22 17:30:35 +0100

    Add batch build of examples

---
+++ build-examples.sh
@@ -0,0 +1,17 @@
+echo "*** Building align-staves ***"
+npm run -s align-staves
+echo "*** Building beaming ***"
+npm run -s beaming
+echo "*** Building chord-symbols ***"
+npm run -s chord-symbols
+echo "*** Building clefs ***"
+npm run -s clefs
+echo "*** Building display-tests ***"
+npm run -s display-tests
+echo "*** Building full-tune ***"
+npm run -s full-tune
+echo "*** Building slurs ***"
+npm run -s slurs
+echo "*** Building thumbnail ***"
+npm run -s thumbnail
+
