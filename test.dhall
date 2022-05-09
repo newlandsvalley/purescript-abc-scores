3b1ce07387d06a35d05d6cd98ddce82a67802abe
John Watson <john.watson@gmx.co.uk>
2022-05-09 09:16:38 +0100

    Replace test-unit with spec

--- test.dhall
+++ test.dhall
@@ -2,5 +2,5 @@ let conf = ./spago.dhall
 
 in conf // {
   sources = conf.sources # [ "test/**/*.purs" ],
-  dependencies = conf.dependencies # [  "console" , "spec" ]
+  dependencies = conf.dependencies # [ "aff", "console" , "spec" ]
 }
--- test/Main.purs
+++ test/Main.purs
@@ -2,7 +2,6 @@ module Test.Main where
 
 import Test.Samples
 
-import Control.Monad.Free (Free)
 import Data.Abc (PitchClass(..))
 import Data.Abc.Parser (parse)
 import Data.Array (head)
@@ -11,23 +10,26 @@ import Data.Maybe (Maybe(..))
 import Data.Rational ((%))
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
+import Effect.Aff (Aff, launchAff_)
 import Prelude (($), (<>), Unit, discard, map, show)
-import Test.Unit (Test, TestF, suite, test, failure)
-import Test.Unit.Assert as Assert
-import Test.Unit.Main (runTest)
 import VexFlow.Abc.Alignment (justifiedScoreConfig)
 import VexFlow.Abc.Beat (exactBeatNumber)
 import VexFlow.Score (createScore)
 import VexFlow.Types (BarSpec, Config, VexScore, MusicSpec(..), defaultConfig)
+import Test.Spec (Spec, describe, it)
+import Test.Spec.Assertions (fail, shouldEqual)
+import Test.Spec.Reporter (specReporter)
+import Test.Spec.Runner (runSpec)
 
 main :: Effect Unit
-main = runTest do
-  configThreadingSuite
-  slursSuite
-  beatSuite
-  beamingSuite
-  typesettingSpaceSuite
-  alignmentSuite
+main = launchAff_ $ runSpec [ specReporter] do 
+  describe "abc-scores" do
+    configThreadingSpec
+    slursSpec
+    beatSpec
+    beamingSpec
+    typesettingSpaceSpec
+    alignmentSpec
 
 initialConfig :: Config
 initialConfig =
@@ -38,18 +40,17 @@ initialConfig =
     , titled = false
     }
 
-assertScoreDepth :: String -> Int -> Test
-assertScoreDepth s expected =
+assertScoreDepth :: String -> Int -> Aff Unit
+assertScoreDepth s expected = do
   case parse s of
-    Right tune ->
+    Right tune -> do
       let
         score = createScore initialConfig tune
         newConfig = justifiedScoreConfig score initialConfig
-      in
-        Assert.equal expected newConfig.height
+      expected `shouldEqual` newConfig.height
 
     Left err ->
-      failure ("parse failed: " <> (show err))
+      fail ("parse failed: " <> (show err))
 
 getFirstBar :: String -> Maybe BarSpec
 getFirstBar s =
@@ -75,170 +76,166 @@ firstBarOfScore vexScore =
       Nothing
 
 -- | check that the configuration changes are threaded through the translation
-configThreadingSuite :: Free TestF Unit
-configThreadingSuite =
-  suite "config threading" do
-    test "meter change (4/4 to 3/4)" do
+configThreadingSpec :: Spec Unit
+configThreadingSpec =
+  describe "config threading" do
+    it "handles meter change (4/4 to 3/4)" do
       let
         initialContext = startAbcContext (Tuple 4 4)
         abcContext = meterChangeTo34 initialContext
       -- key change alters the time signature and beats per beam
-      Assert.equal 3 abcContext.timeSignature.numerator
+      3 `shouldEqual` abcContext.timeSignature.numerator
       -- score item sets the first stave number to 0
-      Assert.equal (Just 0) abcContext.staveNo
-    test "key change (C to G)" do
+      (Just 0) `shouldEqual` abcContext.staveNo
+    it "handles key change (C to G)" do
       let
         initialContext = startAbcContext (Tuple 4 4)
         abcContext = keyChangeToG initialContext
       -- key change alters the time signature and beats per beam
-      Assert.equal G abcContext.keySignature.pitchClass
-    -- this now depends on varianle bar widths
-    test "4 bars width" do
+      G `shouldEqual` abcContext.keySignature.pitchClass
+    -- this now depends on variable bar widths
+    it "calculates width of 4 bars" do
       let
         initialContext = startAbcContext (Tuple 4 4)
         abcContext = accumulateBarWidths initialContext
       -- key change alters the time signature and beats per beam
-      Assert.equal 465 abcContext.accumulatedStaveWidth
+      465 `shouldEqual` abcContext.accumulatedStaveWidth
 
-beamingSuite :: Free TestF Unit
-beamingSuite =
-  suite "beaming" do
-    test "standard 2/4" do
+beamingSpec :: Spec Unit
+beamingSpec =
+  describe "beaming" do
+    it "beams standard 2/4" do
       let
         mFirstBar = getFirstBar "L: 1/16\r\nM: 2/4\r\ne2c2 f2c2\r\n"
-      Assert.equal (Just [ [ 0, 2 ], [ 2, 4 ] ]) $
+      (Just [ [ 0, 2 ], [ 2, 4 ] ]) `shouldEqual`
         map (\b -> b.beamSpecs) mFirstBar
-    test "triplet starting 2/4" do
+    it "beams triplet starting 2/4" do
       let
         mFirstBar = getFirstBar "L: 1/16\r\nM: 2/4\r\n(3d2e2c2 f2c2\r\n"
-      Assert.equal (Just [ [ 0, 3 ], [ 3, 5 ] ]) $
+      (Just [ [ 0, 3 ], [ 3, 5 ] ]) `shouldEqual`
         map (\b -> b.beamSpecs) mFirstBar
-    test "standard 2/4 long introductory note" do
+    it "beams standard 2/4 long introductory note" do
       let
         mFirstBar = getFirstBar "L: 1/16\r\nM: 2/4\r\nd4 f2c2\r\n"
-      Assert.equal (Just [ [ 1, 3 ] ]) $
+      (Just [ [ 1, 3 ] ]) `shouldEqual`
         map (\b -> b.beamSpecs) mFirstBar
-    test "triplet starting 2/4" do
+    it "beams triplet starting 2/4" do
       let
         mFirstBar = getFirstBar "L: 1/16\r\nM: 2/4\r\nf2c2 (3d2e2c2 \r\n"
-      Assert.equal (Just [ [ 0, 2 ], [ 2, 5 ] ]) $
+      (Just [ [ 0, 2 ], [ 2, 5 ] ]) `shouldEqual`
         map (\b -> b.beamSpecs) mFirstBar
-    test "standard 3/4" do
+    it "beams standard 3/4" do
       let
         mFirstBar = getFirstBar "L: 1/16\r\nM: 3/4\r\ne2c2 f2c2 B2A2\r\n"
-      Assert.equal (Just [ [ 0, 2 ], [ 2, 4 ], [ 4, 6 ] ]) $
+      (Just [ [ 0, 2 ], [ 2, 4 ], [ 4, 6 ] ]) `shouldEqual`
         map (\b -> b.beamSpecs) mFirstBar
     -- notes in triplet are too long to be beamed
-    test "3/4 slow triplet start" do
+    it "beams 3/4 slow triplet start" do
       let
         mFirstBar = getFirstBar "L: 1/16\r\nM: 3/4\r\n(3e4c4f4 B2A2\r\n"
-      Assert.equal (Just [ [ 3, 5 ] ]) $
+      (Just [ [ 3, 5 ] ]) `shouldEqual`
         map (\b -> b.beamSpecs) mFirstBar
     -- notes in triplet are too long to be beamed
-    test "3/4 slow triplet end" do
+    it "beams 3/4 slow triplet end" do
       let
         mFirstBar = getFirstBar "L: 1/16\r\nM: 3/4\r\nC4 (3e4c4f4 \r\n"
-      Assert.equal (Just []) $
+      (Just []) `shouldEqual`
         map (\b -> b.beamSpecs) mFirstBar
-    test "split beaming in slow triplet" do
+    it "splits beaming in slow triplet" do
       let
         mFirstBar = getFirstBar "L: 1/16\r\nM: 3/4\r\nC4 (3:2:5d2e2c4f2g2 \r\n"
-      Assert.equal (Just [ [ 1, 3 ], [ 4, 6 ] ]) $
+      (Just [ [ 1, 3 ], [ 4, 6 ] ]) `shouldEqual`
         map (\b -> b.beamSpecs) mFirstBar
-    test "split beaming - tuplet before standard" do
+    it "splits beaming - tuplet before standard" do
       let
         mFirstBar = getFirstBar "L: 1/16\r\nM: 3/4\r\n (3:2:4c4d4e2f2 ABcd \r\n"
-      Assert.equal (Just [ [ 2, 4 ], [ 4, 8 ] ]) $
+      (Just [ [ 2, 4 ], [ 4, 8 ] ]) `shouldEqual`
         map (\b -> b.beamSpecs) mFirstBar
-    test "rest in slow triplet" do
+    it "beams rest in slow triplet" do
       let
         mFirstBar = getFirstBar "L: 1/16\r\nM: 3/4\r\nC4 (3:2:4e4z4f2g2 \r\n"
-      Assert.equal (Just [ [ 3, 5 ] ]) $
+      (Just [ [ 3, 5 ] ]) `shouldEqual`
         map (\b -> b.beamSpecs) mFirstBar
-    test "standard/tuplet overlap 1" do
+    it "beams standard/tuplet overlap 1" do
       let
         mFirstBar = getFirstBar "L: 1/16\r\nM: 4/4\r\ne2c2 (3f2c2d2 e2c2 f2c2\r\n"
-      Assert.equal (Just [ [ 0, 2 ], [ 2, 5 ], [ 5, 9 ] ]) $
+      (Just [ [ 0, 2 ], [ 2, 5 ], [ 5, 9 ] ]) `shouldEqual`
         map (\b -> b.beamSpecs) mFirstBar
-    test "standard/tuplet overlap 2" do
+    it "beams standard/tuplet overlap 2" do
       let
         mFirstBar = getFirstBar "L: 1/16\r\nM: 4/4\r\ne2c2 f2c2 (3f2c2d2 e2c2 \r\n"
-      Assert.equal (Just [ [ 0, 4 ], [ 4, 7 ], [ 7, 9 ] ]) $
+      (Just [ [ 0, 4 ], [ 4, 7 ], [ 7, 9 ] ]) `shouldEqual`
         map (\b -> b.beamSpecs) mFirstBar
-    test "short lead-in" do
+    it "beams short lead-in" do
       let
         mFirstBar = getFirstBar "L: 1/16\r\nM: 3/4\r\ncde\r\n"
-      Assert.equal (Just [ [ 0, 3 ] ]) $
+      (Just [ [ 0, 3 ] ]) `shouldEqual`
         map (\b -> b.beamSpecs) mFirstBar
     -- common time optimisations
-    test "common time optimisation both halves" do
+    it "handles common time optimisation both halves" do
       let
         mFirstBar = getFirstBar "L: 1/16\r\nM: 4/4\r\ne2c2 f2c2 e2c2 f2c2\r\n"
-      Assert.equal (Just [ [ 0, 4 ], [ 4, 8 ] ]) $
+      (Just [ [ 0, 4 ], [ 4, 8 ] ]) `shouldEqual`
         map (\b -> b.beamSpecs) mFirstBar
-    test "common time optimisation first half" do
+    it "handles common time optimisation first half" do
       let
         mFirstBar = getFirstBar "L: 1/16\r\nM: 4/4\r\ne2c2 f2c2 e4 f2c2\r\n"
-      Assert.equal (Just [ [ 0, 4 ], [ 5, 7 ] ]) $
+      (Just [ [ 0, 4 ], [ 5, 7 ] ]) `shouldEqual`
         map (\b -> b.beamSpecs) mFirstBar
-    test "common time optimisation second half" do
+    it "handles common time optimisation second half" do
       let
         mFirstBar = getFirstBar "L: 1/16\r\nM: 4/4\r\ne4 f2c2 e2c2 f2c2 \r\n"
-      Assert.equal (Just [ [ 1, 3 ], [ 3, 7 ] ]) $
+      (Just [ [ 1, 3 ], [ 3, 7 ] ]) `shouldEqual`
         map (\b -> b.beamSpecs) mFirstBar
 
-slursSuite :: Free TestF Unit
-slursSuite =
-  suite "slurs" do
-    test "simple" do
+slursSpec :: Spec Unit
+slursSpec =
+  describe "slurs" do
+    it "handles simple slurs" do
       let
         mFirstBar = getFirstBar "CDE (FG)A Bcd efg\r\n"
-      Assert.equal (Just [ { from: 3, to: 4 } ]) $
+      (Just [ { from: 3, to: 4 } ]) `shouldEqual`
         map (\b -> b.curves) mFirstBar
-    test "nested" do
+    it "handles nested slurs" do
       let
         mFirstBar = getFirstBar "CDE (F(GA) B)cd efg\r\n"
-      Assert.equal (Just [ { from: 4, to: 5 }, { from: 3, to: 6 } ]) $
+      (Just [ { from: 4, to: 5 }, { from: 3, to: 6 } ]) `shouldEqual`
         map (\b -> b.curves) mFirstBar
-    test "unmatched start" do
+    it "handles an unmatched start" do
       let
         mFirstBar = getFirstBar "(CDE (FG)A Bcd efg\r\n"
-      Assert.equal (Just [ { from: 3, to: 4 } ]) $
+      (Just [ { from: 3, to: 4 } ]) `shouldEqual`
         map (\b -> b.curves) mFirstBar
-    test "unmatched end" do
+    it "handles an unmatched end" do
       let
         mFirstBar = getFirstBar "CDE (FG)A Bcd) efg\r\n"
-      Assert.equal (Just [ { from: 3, to: 4 } ]) $
+      (Just [ { from: 3, to: 4 } ]) `shouldEqual`
         map (\b -> b.curves) mFirstBar
 
-beatSuite :: Free TestF Unit
-beatSuite =
-  suite "beats" do
-    test "beat 0" do
-      Assert.equal Nothing
-        -- Assert.equal (Just { beatNumber: 0, noteIndex: 0})
-        $ exactBeatNumber (0 % 2) (1 % 4) 0
-    test "beat 1" do
-      Assert.equal (Just { beatNumber: 1, noteIndex: 1 })
-        $ exactBeatNumber (1 % 4) (1 % 4) 1
-    test "beat 2" do
-      Assert.equal (Just { beatNumber: 2, noteIndex: 1 })
-        $ exactBeatNumber (1 % 2) (1 % 4) 1
-    test "beat 3" do
-      Assert.equal (Just { beatNumber: 3, noteIndex: 2 })
-        $ exactBeatNumber (3 % 4) (1 % 4) 2
-    test "off beat" do
-      Assert.equal Nothing
-        $ exactBeatNumber (2 % 5) (1 % 4) 1
+beatSpec :: Spec Unit
+beatSpec =
+  describe "beats" do
+    it "handles beat 0" do
+      Nothing `shouldEqual` exactBeatNumber (0 % 2) (1 % 4) 0
+    it "calculates beat 1" do
+      (Just { beatNumber: 1, noteIndex: 1 }) `shouldEqual`
+        exactBeatNumber (1 % 4) (1 % 4) 1
+    it "calculates beat 2" do
+      (Just { beatNumber: 2, noteIndex: 1 }) `shouldEqual`
+        exactBeatNumber (1 % 2) (1 % 4) 1
+    it "calculates beat 3" do
+      (Just { beatNumber: 3, noteIndex: 2 }) `shouldEqual`
+        exactBeatNumber (3 % 4) (1 % 4) 2
+    it "recognizes off beat" do
+      Nothing `shouldEqual` exactBeatNumber (2 % 5) (1 % 4) 1
 
-typesettingSpaceSuite :: Free TestF Unit
-typesettingSpaceSuite =
-  suite "typesetting spaces" do
-    test "pair of ys" do
+typesettingSpaceSpec :: Spec Unit
+typesettingSpaceSpec =
+  describe "typesetting spaces" do
+    it "handles a pair of ys" do
       let
         mFirstBar = getFirstBar "(3c2d2e2 y f2g2 c2f2 y (3g2a2b2 \r\n"
-      Assert.equal (Just [ 3, 7 ]) $
-        map getSpaces mFirstBar
+      (Just [ 3, 7 ]) `shouldEqual` (map getSpaces mFirstBar)
   where
   getSpaces :: BarSpec -> Array Int
   getSpaces bs =
@@ -247,9 +244,9 @@ typesettingSpaceSuite =
     in
       ms.typesettingSpaces
 
-alignmentSuite :: Free TestF Unit
-alignmentSuite =
-  suite "alignment" do
-    test "borddajnsijn" do
+alignmentSpec :: Spec Unit
+alignmentSpec =
+  describe "alignment" do
+    it "calculates depth of borddajnsijn" do
       assertScoreDepth borddajnsijn 188
 
