1da55b1675d0fc53e218beb784c87f5c675e2816
John Watson <john.watson@gmx.co.uk>
2022-06-12 14:26:11 +0100

    Small change to full-score example

--- examples/full-tune/src/Main.purs
+++ examples/full-tune/src/Main.purs
@@ -3,7 +3,7 @@ module Examples.FullTune.Main where
 import Prelude (bind, pure)
 import Effect (Effect)
 import Data.Either (Either(..))
-import VexFlow.Score (renderTune, initialiseCanvas)
+import VexFlow.Score (renderFinalTune, initialiseCanvas)
 import VexFlow.Types (Config, defaultConfig)
 import VexFlow.Abc.Utils (canvasHeight)
 import Data.Abc.Parser (parse)
@@ -27,6 +27,9 @@ main =
   case (parse chordSymbolExample) of
     Right abcTune -> do
       renderer <- initialiseCanvas config
+      {- this renders with staves extended to the RHS and with confog width repected
       renderTune config renderer abcTune
+      -}
+      renderFinalTune config renderer abcTune
     _ ->
       pure false
